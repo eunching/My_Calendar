7acaacb5eac8de84eb6c57220d771380e629f0f7
eunching <43661212+eunching@users.noreply.github.com>
2020-01-20 21:41:32 +0900

    add calendar and progress bar

--- app/src/main/java/com/android/mycalendar/MainActivity.java
+++ app/src/main/java/com/android/mycalendar/MainActivity.java
@@ -7,10 +7,18 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,17 +26,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Button UpAndDownBtn = findViewById(R.id.UpAndDown);
+        Button.OnClickListener onClickListener = new Button.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view){
+                switch (view.getId()) {
+                    case R.id.UpAndDown:
+                        String buffer = UpAndDownBtn.getText().toString();
+                        if(buffer.equals("up")){
+                            UpAndDownBtn.setText("down");
+                        }else{
+                            UpAndDownBtn.setText("up");
+                        }
+
+                        break;
+                }
+
             }
-        });
+        };
+
+
+        UpAndDownBtn.setOnClickListener(onClickListener);
+
     }
 
     @Override
